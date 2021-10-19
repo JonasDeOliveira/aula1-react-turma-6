e7579ce9424a64e36cc048aed275e4fb8545b147
JonasDeOliveira <jonas.doliver@gmail.com>
2021-10-19 15:37:38 -0300

    exemplos ate estado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import React from "react";
+import Contador from "./components/Contator";
+// import Primeiro from './components/Primeiro'
+// import CompPadrao, { Component1, Component2 } from './components/Multi'
+// import Estilos from './components/Estilos'
+// import MinMax from "./components/MinMax";
+// import Titulo from './components/Titulo'
+// import Button from "./components/Button/Button";
 
 function App() {
   return (
-    <h1>Olá mundo</h1>
-  );
+    <>
+    <Contador inicial={0} passo={5}/>
+    <Contador inicial={100} passo={10}/>
+      {/* <Button type="primary" text="Cadastrar" acao="cadastrar"/>  
+      <Button type="secundary" text="Voltar" acao="voltar"/>       */}
+      {/* <Titulo principal="Olá" secundario="tudo bem"/>
+      <Titulo principal="Teste"/>
+      <Titulo principal="Teste de estilo"/> */}
+      {/* <MinMax min="2" max="10"/>
+      <MinMax min={4} max={15}/> */}
+      {/* <Estilos/>
+      <CompPadrao/>
+      <Component1/>
+      <Component2/>
+      <Primeiro/> */}
+    </>
+  
+  )
 }
 
 export default App;
